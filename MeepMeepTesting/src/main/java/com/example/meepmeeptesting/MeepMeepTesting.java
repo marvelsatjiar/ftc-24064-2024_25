a8710d050f092dc7de6c5ff8bbcf784ecda3eeff
TheRealRamo1234 <omarnebula14@gmail.com>
2024-11-14 01:44:13 -0500

    Complete Auton Paths
    - Specimen side (4+0)
    - Sample Side (1+3)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,24 +9,34 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(600);
-        boolean isSpecimenSide = true;
+        MeepMeep meepMeep = new MeepMeep(1200);
+        boolean isSpecimenSide = false;
 
         RoadRunnerBotEntity drive = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 11.5)
                 .build();
 
-        TrajectoryActionBuilder builder = drive.getDrive().actionBuilder(new Pose2d(10,-63,Math.toRadians(-270)));
-        if (!isSpecimenSide) {
+        Pose2d startPose;
+        if (isSpecimenSide)
+            startPose = new Pose2d(10,-63,Math.toRadians(-270));
+        else
+            startPose = new Pose2d(-10,-63,Math.toRadians(-270));
 
+
+
+
+        TrajectoryActionBuilder builder = drive.getDrive().actionBuilder(startPose);
+        if (!isSpecimenSide) {
             builder = scoreSpecimen(builder);
-            builder = scoreSample1(builder);
-            builder = scoreSample2(builder);
-            builder = scoreSample3(builder);
+            builder = scoreSamples(builder);
+            builder = samplePark(builder);
         } else {
             builder = scoreSpecimen(builder);
-            builder = pushSample(builder);
+            builder = giveSamples(builder);
+            builder = scoreAllSpecimens(builder);
+            builder = specimenPark(builder);
+
 
 
 
@@ -42,78 +52,74 @@ public class MeepMeepTesting {
                 .start();
     }
 
-    private static TrajectoryActionBuilder pushSample(TrajectoryActionBuilder builder) {
+    private static TrajectoryActionBuilder specimenPark(TrajectoryActionBuilder builder) {
         builder = builder
-                .lineToY(-40)
-                .strafeTo(new Vector2d(12 ,-40))
-                .setTangent(Math.toRadians(90))
-                .splineTo(new Vector2d(48, -10), Math.toRadians(270))
-//                .splineTo(new Vector2d(29, -10), Math.toRadians(90))
-
-
-
-
-
+                .lineToYLinearHeading(-48,Math.toRadians(-40))
         ;
 
-
-
-
         return builder;
     }
 
-    private static TrajectoryActionBuilder scoreSample2(TrajectoryActionBuilder builder) {
+    private static TrajectoryActionBuilder samplePark(TrajectoryActionBuilder builder) {
         builder = builder
-                .lineToXSplineHeading(-53,Math.toRadians(90))
-                .setTangent(90)
-                .lineToY(-43)
-                .waitSeconds(1)
-                .lineToYSplineHeading(-50,Math.toRadians(45))
-                .waitSeconds(1)
-
-
-
-                ;
-        return builder;
-
-    }
-
-    private static TrajectoryActionBuilder scoreSample3(TrajectoryActionBuilder builder) {
-        builder = builder
-//                .lineToYSplineHeading(-40,Math.toRadians(115))
-                .lineToXSplineHeading(-55,Math.toRadians(115))
-//                .waitSeconds(1)
-                .setTangent(90)
-                .lineToY(-43)
-                .waitSeconds(1)
-                .lineToYSplineHeading(-50,Math.toRadians(55))
-
-
-
-
-                ;
-
-
-
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(-25,-10,Math.toRadians(180)),Math.toRadians(0))
+        ;
         return builder;
     }
 
-    private static TrajectoryActionBuilder scoreSample1(TrajectoryActionBuilder builder) {
+    private static TrajectoryActionBuilder scoreAllSpecimens(TrajectoryActionBuilder builder) {
         builder = builder
-//                .setTangent(-90)
-                .lineToY(-40)
-                .setTangent(Math.toRadians(270))
-                .splineToConstantHeading(new Vector2d(-48, -48), Math.toRadians(270))
-//                .setTangent(270)
-                .waitSeconds(1)
-                .setTangent(45)
-                .splineToLinearHeading(new Pose2d(-55,-53,45),-270)
-                .waitSeconds(1)
+                .setTangent(Math.toRadians(180))
+                .splineToConstantHeading(new Vector2d(26,-38),Math.toRadians(180))
+                .splineToConstantHeading(new Vector2d(7,-36),Math.toRadians(180))
+//                .splineToSplineHeading(new Pose2d(10,-36,Math.toRadians(270)),Math.toRadians(180))
+                .setTangent(-45)
+                .lineToYLinearHeading(-45,Math.toRadians(-40))
+                .lineToYLinearHeading(-36,Math.toRadians(270))
+                .lineToYLinearHeading(-45,Math.toRadians(-40))
+                .lineToYLinearHeading(-36,Math.toRadians(270))
+                .lineToYLinearHeading(-45,Math.toRadians(-40))
+                .lineToYLinearHeading(-36,Math.toRadians(270))
+//                .lineToYLinearHeading(-45,Math.toRadians(-40))
+//                .lineToYLinearHeading(-36,Math.toRadians(270))
+        ;
+        return builder;
 
 
+    }
+    private static TrajectoryActionBuilder giveSamples(TrajectoryActionBuilder builder) {
+        builder = builder
+                .setTangent(Math.toRadians(-20))
+                .splineToConstantHeading(new Vector2d(26,-38),Math.toRadians(0))
+                .splineToSplineHeading(new Pose2d(35,-36,Math.toRadians(35)),Math.toRadians(35))
+        //extend intake ~5in
+                .turn(Math.toRadians(-70))
+                //give sample 2 below
+                .splineToSplineHeading(new Pose2d(47,-36,Math.toRadians(35)),Math.toRadians(35))
+                .turn(Math.toRadians(-80))
+        //give sample 3 below
+                .turn(Math.toRadians(65))
+                .turn(Math.toRadians(-110))
+        ;
+        return builder;
+    }
 
-
-
+    private static TrajectoryActionBuilder scoreSamples(TrajectoryActionBuilder builder) {
+        builder = builder
+                .setTangent(180)
+//                .splineToConstantHeading(new Vector2d(-35,-38),Math.toRadians(180))
+                .splineToSplineHeading(new Pose2d(-48,-45,Math.toRadians(-270)),Math.toRadians(180))
+                .setTangent(Math.toRadians(-135))
+                .splineToLinearHeading(new Pose2d(-56,-56,Math.toRadians(45)),Math.toRadians(-135))
+                .setTangent(Math.toRadians(110))
+                .splineToLinearHeading(new Pose2d(-58,-50,Math.toRadians(90)),Math.toRadians(110))
+                .setTangent(Math.toRadians(290))
+                .splineToLinearHeading(new Pose2d(-56,-56,Math.toRadians(45)),Math.toRadians(290))
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(-54,-46,Math.toRadians(127)),Math.toRadians(127))
+                .setTangent(Math.toRadians(255))
+                .splineToLinearHeading(new Pose2d(-56,-56,Math.toRadians(45)),Math.toRadians(255))
         ;
         return builder;
     }
@@ -121,7 +127,7 @@ public class MeepMeepTesting {
     private static TrajectoryActionBuilder scoreSpecimen(TrajectoryActionBuilder builder) {
         builder = builder
                 .lineToY(-35)
-                .waitSeconds(1);
+                ;
 
         return builder;
     }
